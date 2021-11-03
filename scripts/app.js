4da479e39cb4a17a0cd7d5407cb2e589c1fe3062
shubhamfuloria <sfuloria10200@gmail.com>
2021-11-03 18:07:39 +0530

    first_commit

---
+++ scripts/app.js
@@ -0,0 +1,127 @@
+let board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+function Game(p1, p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.currTurnId = 0;
+}
+
+Game.prototype.nextTurn = function () {
+    this.currTurnId = Number(!this.currTurnId);
+}
+
+//this function will return the winner player object
+Game.prototype.ifWon = function () {
+    //row check 
+    for (let el of board) {
+        if (isEveryElementSame(el)) {
+            return true;
+        }
+    }
+    //column check
+    for (let i = 0; i < 3; i++) {
+        //create an array from columns
+        let ar = board.map(function (array) {
+            return array[i];
+        })
+        if (isEveryElementSame(ar)) {
+            return true;
+        }
+    }
+    //diagonals check
+    let left_diagonal = [];
+    let right_diagonal = [];
+
+    for (let i = 0; i < 3; i++) {
+        left_diagonal.push(board[i][i])
+        right_diagonal.push(board[i][2 - i]);
+    }
+    if (isEveryElementSame(left_diagonal) || isEveryElementSame(right_diagonal)) {
+        return true;
+    }
+    return false;
+}
+Game.prototype.move = function (cellId) {
+    let row = Number(cellId[0]);
+    let col = Number(cellId[1]);
+
+    let sign = this.currTurnId === 0 ? 'X' : 'O'
+    if (board[row][col] === null) { //should be a valid move
+        board[row][col] = sign;
+        if (this.ifWon()) {
+            //currPlayer has won
+            console.log(this.getCurrentPlayer().name + " has won the match :)")
+        }
+        currGame.nextTurn();
+    }
+}
+
+Game.prototype.getCurrentPlayer = function () {
+    if (this.currTurnId == 0) {
+        return p1;
+    } else {
+        return p2;
+    }
+}
+
+class Player {
+    constructor(name, sign, id) {
+        this.name = name;
+        this.id = id;
+        this.score = 0;
+        this.sign = sign;
+    }
+}
+//utility function
+function isEveryElementSame(array) {
+    return array.every(el => el === array[0] && el !== null);
+}
+
+//trying the game
+var p1, p2;
+var currGame;
+
+
+
+/*
+1. Created Players with sign and id and Name
+2. turn logic implemented
+3. ifWin code implemented
+4. Implement player moves
+*/
+const newGameBtnEl = document.querySelector('.new-game')
+const formEl = document.querySelector('.popup')
+const startBtnEl = document.querySelector('.start-btn');
+const player1Input = document.getElementById('player-1')
+const player2Input = document.getElementById('player-2')
+const cells = document.querySelectorAll('.cell');
+
+
+function initialize() {
+    const player1 = player1Input.value;
+    const player2 = player2Input.value;
+    
+    //initializing players
+    p1 = new Player(player1, 'X', 0);
+    p2 = new Player(player2, 'O', 1);
+
+    currGame = new Game(p1, p2);
+    console.log(currGame);
+    formEl.classList.remove('open');
+}
+
+function openPopup() {
+    //open popup    
+    console.log("Clicked New Game")
+    formEl.classList.add('open')
+}
+
+
+cells.forEach( cell => console.log(cell))
+
+newGameBtnEl.addEventListener('click', openPopup)
+startBtnEl.addEventListener('click', initialize)
